fe2197bb247b3922281d8b6a93490d561a3a3dfc
RutujaH <handerutuja21@gmail.com>
2024-04-07 02:16:38 +0530

    first Commit

---
+++ firebase-login-master/firebase-login-master/js/config/config.js
@@ -0,0 +1,17 @@
+/**
+ * @file config.js
+ * @author Sanjay Sunil
+ * @license MIT
+ */
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD5r9oclUOVUbn4rcWCZoeSpUX8jZ24fxw",
+    authDomain: "tours-a57d0.firebaseapp.com",
+    databaseURL: "https://tours-a57d0-default-rtdb.firebaseio.com",
+    projectId: "tours-a57d0",
+    storageBucket: "tours-a57d0.appspot.com",
+    messagingSenderId: "709046136965",
+    appId: "1:709046136965:web:fef29838372ee840ccf9ec"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
